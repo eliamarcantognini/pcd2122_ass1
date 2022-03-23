a4613380a640b2d4b14b149141759a5c2804c5df
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-03-23 19:42:28 +0100

    Modify prints and take bodies in constructor

--- src/main/java/concurrent/model/BodyAgent.java
+++ src/main/java/concurrent/model/BodyAgent.java
@@ -1,26 +1,34 @@
 package concurrent.model;
 
+import java.util.List;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 public class BodyAgent extends Thread{
     private final Body body;
     private final CyclicBarrier cyclicBarrier;
+    private final List<Body> bodies;
 
-    public BodyAgent(final Body body, final CyclicBarrier cyclicBarrier){
+
+    public BodyAgent(final Body body, final List<Body> bodies, final CyclicBarrier cyclicBarrier){
         this.body = body;
         this.cyclicBarrier = cyclicBarrier;
+        this.bodies = bodies;
     }
 
     @Override
     public void run() {
-        System.out.println("Thread of: " + this.body.getId() + " running...");
-        try {
-            this.cyclicBarrier.await();
-        } catch (Exception e) {
-            System.out.println("Aiha?");
-            System.exit(-1);
+        for (int i = 0; i<2; i++) {
+            System.out.println("Thread of: " + this.body.getId() + " BBBBB wait...");
+            try {
+                this.cyclicBarrier.await();
+            } catch (Exception e) {
+                System.out.println("Aiha?");
+                System.exit(-1);
+            }
+
+            //System.out.println(this.bodies.toString());
+            System.out.println("Thread of: " + this.body.getId() + " AA wait...");
         }
-        System.out.println("Thread of: " + this.body.getId() + " after await...");
     }
 }

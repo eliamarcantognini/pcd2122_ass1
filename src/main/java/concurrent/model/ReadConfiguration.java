d0a6e74e5d19b2bf7f3873fbee3b3352cfc0bedf
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-04-09 18:53:35 +0200

    Add getter for boundaries of field

--- src/main/java/concurrent/model/ReadConfiguration.java
+++ src/main/java/concurrent/model/ReadConfiguration.java
@@ -10,6 +10,10 @@ public class ReadConfiguration {
 
     private static final String ITERATIONS_QUANTITY_FIELD_NAME = "iterationsQuantity";
     private static final String BODIES_QUANTITY_FIELD_NAME = "bodiesQuantity";
+    private static final String UPPER_BOUNDARY_FIELD_NAME = "upperBoundary";
+    private static final String LOWER_BOUNDARY_FIELD_NAME = "lowerBoundary";
+    private static final String RIGHTER_BOUNDARY_FIELD_NAME = "righterBoundary";
+    private static final String LEFTER_BOUNDARY_FIELD_NAME = "lefterBoundary";
     private final Properties properties;
 
     public ReadConfiguration(final String fileName) throws IOException {
@@ -32,4 +36,21 @@ public class ReadConfiguration {
     public int iterationsQuantity(){
         return Integer.parseInt(this.properties.getProperty(ReadConfiguration.ITERATIONS_QUANTITY_FIELD_NAME));
     }
+
+    public int upperBoundary(){
+        return Integer.parseInt(this.properties.getProperty(ReadConfiguration.UPPER_BOUNDARY_FIELD_NAME));
+    }
+
+    public int lowerBoundary(){
+        return Integer.parseInt(this.properties.getProperty(ReadConfiguration.LOWER_BOUNDARY_FIELD_NAME));
+    }
+
+    public int righterBoundary(){
+        return Integer.parseInt(this.properties.getProperty(ReadConfiguration.RIGHTER_BOUNDARY_FIELD_NAME));
+    }
+
+    public int lefterBoundary(){
+        return Integer.parseInt(this.properties.getProperty(ReadConfiguration.LEFTER_BOUNDARY_FIELD_NAME));
+    }
+
 }

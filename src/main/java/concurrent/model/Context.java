ae9b3d470f897a48b3a2fde6a2516b7600d02829
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-04-10 19:14:26 +0200

    Modify Context javadoc

--- src/main/java/concurrent/model/Context.java
+++ src/main/java/concurrent/model/Context.java
@@ -6,50 +6,88 @@ package concurrent.model;
  * one to read and the other to write.
  *
  * @see concurrent.model.BodiesSharedList
+ * @see concurrent.model.Boundary
  */
 
 public class Context {
 
     private final double dt;
     private final Boundary boundary;
+    private boolean keepWorking;
     private BodiesSharedList writeSharedList;
     private BodiesSharedList readSharedList;
-    private boolean keepWorking;
 
+    /**
+     * Create the context with boundaries and deltaT passed
+     * @param boundary - boundaries of two-dimensional world
+     * @param dt - the deltaT, that is quantity time added to virtual time each iteration
+     */
     public Context(final Boundary boundary, final double dt) {
         this.boundary = boundary;
         this.dt = dt;
         this.restartContext();
     }
 
+    /**
+     * Restart context reinitializing lists and keepWorking flag
+     */
     public void restartContext() {
         this.keepWorking = true;
         this.writeSharedList = new BodiesSharedList();
         this.readSharedList = new BodiesSharedList();
     }
 
+    /**
+     * Get boundaries of two-dimensional world
+     *
+     * @return - boundaries of two-dimensional world
+     */
     public Boundary getBoundary() {
         return boundary;
     }
 
+    /**
+     * Check if simulation is running yet
+     *
+     * @return true if simulation is running yet
+     */
     public boolean isKeepWorking() {
         return keepWorking;
     }
 
+    /**
+     * Set if simulation can run
+     *
+     * @param keepWorking - to set if simulation can run
+     */
     public void setKeepWorking(boolean keepWorking) {
         this.keepWorking = keepWorking;
     }
 
+    /**
+     * Get the list used to write new bodies
+     *
+     * @return the list used to write new bodies
+     */
     public BodiesSharedList getWriteSharedList() {
         return writeSharedList;
     }
 
+    /**
+     * Get the list used to read bodies
+     *
+     * @return the list used to read bodies
+     */
     public BodiesSharedList getReadSharedList() {
         return readSharedList;
     }
 
+    /**
+     * Get the deltaT of the simulation
+     *
+     * @return the deltaT, that is quantity time added to virtual time each iteration
+     */
     public double getDT() {
         return this.dt;
     }
-
 }

54601e151a345f7de033796b37afb453f9aae35e
Michele Bachetti <michele.bachetti@studio.unibo.it>
2022-04-10 13:16:35 +0200

    Add a timer to PrinterView to test if it's work correctly

--- src/main/java/concurrent/view/PrinterView.java
+++ src/main/java/concurrent/view/PrinterView.java
@@ -1,15 +1,17 @@
 package concurrent.view;
 
+import concurrent.controller.GUIListener;
 import concurrent.model.Body;
 import concurrent.model.Boundary;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class PrinterView implements View{
     @Override
     public void display(List<Body> bodies, double vt, long iter, Boundary bounds) {
-        System.out.println(iter);
+        System.out.println(bodies);
     }
 
     @Override
@@ -20,5 +22,15 @@ public class PrinterView implements View{
     @Override
     public void setStartEnabled(Boolean enabled) {
 
+    }
+
+    @Override
+    public void addListener(GUIListener listener) {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                listener.eventPerformed("start");
+            }
+        }, 3000);
     }
 }

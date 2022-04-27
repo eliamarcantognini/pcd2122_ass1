39f42e522e14b3030a00874b59496ee6720ccbaa
Michele Bachetti <michele.bachetti@studio.unibo.it>
2022-04-27 18:13:14 +0200

    Moved the logic of the agents into a task class

---
+++ src/main/java/concurrent/model/UpdateTask.java
@@ -0,0 +1,49 @@
+package concurrent.model;
+
+public class UpdateTask implements Runnable {
+
+    private final Body body;
+    private final BodiesSharedList readSharedList;
+    private final BodiesSharedList writeSharedList;
+    private final Context context;
+
+    public UpdateTask(Body body, Context context) {
+        this.body = new Body(body);
+        this.context = context;
+        this.readSharedList = this.context.getReadSharedList();
+        this.writeSharedList = this.context.getWriteSharedList();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Sono task, mi faccio i conti");
+        /* compute total force on bodies */
+        V2d totalForce = computeTotalForceOnBody(body);
+        /* compute instant acceleration */
+        V2d acc = new V2d(totalForce).scalarMul(1.0 / body.getMass());
+        /* update velocity */
+        body.updateVelocity(acc, this.context.getDT());
+        body.updatePos(this.context.getDT());
+        body.checkAndSolveBoundaryCollision(context.getBoundary());
+        writeSharedList.updateBody(body);
+        System.out.println("sono task, ho fatto i conti");
+    }
+
+    private V2d computeTotalForceOnBody(Body body) {
+        V2d totalForce = new V2d(0, 0);
+        /* compute total repulsive force */
+        for (Body otherBody : this.readSharedList.getBodies()) {
+            if (!body.equals(otherBody)) {
+                try {
+                    V2d forceByOtherBody = body.computeRepulsiveForceBy(otherBody);
+                    totalForce.sum(forceByOtherBody);
+                } catch (Exception ignored) {
+                }
+            }
+        }
+        /* add friction force */
+        totalForce.sum(body.getCurrentFrictionForce());
+
+        return totalForce;
+    }
+}

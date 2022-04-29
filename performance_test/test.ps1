62981df86d4916dbf43eb0bdbd8c273f707a65b4
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-04-29 20:49:44 +0200

    Add script to automate tests

---
+++ performance_test/test.ps1
@@ -0,0 +1,14 @@
+# nB = nBodies
+# nS = nSteps
+#                   nB  nS  
+java -jar .\app.jar 100 1000
+java -jar .\app.jar 100 10000
+java -jar .\app.jar 100 50000
+
+java -jar .\app.jar 1000 1000
+java -jar .\app.jar 1000 10000
+java -jar .\app.jar 1000 50000
+
+java -jar .\app.jar 5000 1000
+java -jar .\app.jar 5000 10000
+java -jar .\app.jar 5000 50000

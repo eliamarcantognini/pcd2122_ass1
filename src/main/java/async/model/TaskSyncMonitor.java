cad33b249adb555ab39f325cb640499989496b79
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-04-30 18:32:26 +0200

    Add javadoc to TaskSyncMonitor

--- src/main/java/async/model/TaskSyncMonitor.java
+++ src/main/java/async/model/TaskSyncMonitor.java
@@ -1,14 +1,26 @@
 package async.model;
 
+/**
+ * Class that implements a monitor used to synchronize tasks.
+ */
 public class TaskSyncMonitor {
 
     private final long bodyToWait;
     private long counter = 0;
 
+    /**
+     * Constructor of monitor that allows to specify number of call of {@link #countMeIn()}
+     * necessary before unlock monitor.
+     *
+     * @param bodyToWait number of call of {@link #countMeIn()} necessary before unlock monitor
+     */
     public TaskSyncMonitor(long bodyToWait) {
         this.bodyToWait = bodyToWait;
     }
 
+    /**
+     * Increase by one monitor's counter
+     */
     public synchronized void countMeIn() {
         counter++;
         if (counter >= bodyToWait) {
@@ -16,12 +28,22 @@ public class TaskSyncMonitor {
         }
     }
 
+    /**
+     * Wait monitor's counter reach value specified in constructor.
+     *
+     * @throws InterruptedException as in {@link Object#wait()}
+     *
+     * @see Object#wait()
+     */
     public synchronized void awaitCompletion() throws InterruptedException {
         while (counter < bodyToWait) {
             wait();
         }
     }
 
+    /**
+     * Reset monitor's counter.
+     */
     public synchronized void reset() {
         counter = 0;
     }
